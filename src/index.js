2b6385d190515b958a3837211ec1a7d3cd7d7eb9
Catherine <catherineep4189@gmail.com>
2020-01-08 21:00:54 +0000

    additional notes

--- src/index.js
+++ src/index.js
@@ -15,14 +15,22 @@ class Square extends React.Component {
 
     render() {
       return (
-        <button className="square" onClick={function() { alert('click'); }}>
-          {this.props.value}
+        <button className="square" 
+         onClick={() => this.setState({value: 'X'})}>
+          {this.state.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+      constructor(props) {
+          super(props);
+          this.state = {
+              square: Array(9).fill(null),
+          };
+      }
+      
     renderSquare(i) {
       return <Square value={i} />;
     }
